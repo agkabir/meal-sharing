bcc09895deca7901a4814fbc091a4856e89dfc4b
mkabir <ag.kabir@gmail.com>
2023-03-11 00:09:05 +0100

    React1-week3 Meal sharing HW first commit

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,7 +1,9 @@
 const app = require("./app");
 
-const port = parseInt(process.env.CLIENT_PORT, 10) || process.env.API_PORT;
+const port =
+  parseInt(process.env.PORT, 10) || parseInt(process.env.API_PORT, 10);
 
 app.listen(port, () => {
   console.log(`Backend api available at ${process.env.API_PATH}`);
+  console.log(`Server running  at ${port}`);
 });
